93bc3213b98019dd5ed7791c03eb3419aa36e85a
Wolfy <yannnonoficiel@gmail.com>
2023-01-28 11:38:56 +0100

    .

--- Packages/NKE/NKE.js
+++
@@ -1,8 +0,0 @@
-document.body.innerHTML = "";
-function ShowText(Text){
-    var TextElement = document.createElement("p");
-    TextElement.innerHTML = Text;
-    TextElement.setAttribute("style" , "font-family:'VGA';color:white;");
-    document.appendChild(TextElement)
-}
-ShowText("Test !")
