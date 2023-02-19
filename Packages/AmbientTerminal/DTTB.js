27320eb7582950e56762ea6ac9e550a7824abc66
Wolfy <yannnonoficiel@gmail.com>
2023-02-19 14:57:01 +0100

    FIX

--- Packages/AmbientTerminal/DTTB.js
+++
@@ -1,4 +0,0 @@
-Shell.document.querySelector(".Taskbar").style.width = "calc(100% - 15px)";
-Shell.document.querySelector(".Taskbar").style.margin = "9px";
-Shell.document.querySelector(".Taskbar").style.borderRadius = "9px";
-Shell.document.querySelector(".Taskbar").style.backgroundColor = "#21212587";
--- Packages/AmbientTerminal/README.md
+++
@@ -1,5 +0,0 @@
-# DockTTB !
-## Installation
-For install DockTTB on your computer , you have to install the package use `apt install DockTTB` !
-## Utilisation
-At normal installation , DockTTB runs everytime you launch the system , but if there is not , use the command `Dock` !
